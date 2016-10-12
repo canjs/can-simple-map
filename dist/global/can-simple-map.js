d50056bb1973c173d626e25925207495770e6e54
John Gardner <john@ardea.io>
2016-10-12 17:00:33 -0500

    3.0.0-pre.5

---
+++ dist/global/can-simple-map.js
@@ -0,0 +1,165 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var useDefault = function(mod){
+		if(!mod || !mod.__esModule) return false;
+		var esProps = { __esModule: true, "default": true };
+		for(var p in mod) {
+			if(!esProps[p]) return false;
+		}
+		return true;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			if(useDefault(result)) {
+				result = result["default"];
+			}
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-simple-map@3.0.0-pre.4#can-simple-map*/
+define('can-simple-map', function (require, exports, module) {
+    var Construct = require('can-construct');
+    var canBatch = require('can-event/batch/batch');
+    var canEvent = require('can-event');
+    var assign = require('can-util/js/assign/assign');
+    var types = require('can-util/js/types/types');
+    var Observation = require('can-observation');
+    var SimpleMap = Construct.extend({
+        setup: function () {
+            this._data = {};
+        },
+        init: function (initialData) {
+            this.attr(initialData);
+        },
+        attr: function (prop, value) {
+            var self = this;
+            if (arguments.length > 1) {
+                var old = this._data[prop];
+                this._data[prop] = value;
+                canBatch.trigger.call(this, prop, [
+                    value,
+                    old
+                ]);
+            } else if (typeof prop === 'object') {
+                Object.keys(prop).forEach(function (key) {
+                    self.attr(key, prop[key]);
+                });
+            } else {
+                if (prop !== 'constructor') {
+                    Observation.add(this, prop);
+                    return this._data[prop];
+                }
+                return this.constructor;
+            }
+        }
+    });
+    assign(SimpleMap.prototype, canEvent);
+    var oldIsMapLike = types.isMapLike;
+    types.isMapLike = function (obj) {
+        if (obj instanceof SimpleMap) {
+            return true;
+        }
+        return oldIsMapLike.call(this, obj);
+    };
+    if (!types.DefaultMap) {
+        types.DefaultMap = SimpleMap;
+    }
+    module.exports = SimpleMap;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
