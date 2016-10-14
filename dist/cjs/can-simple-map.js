859413a88291fdf8be745fae749261c1a74c7838
David Luecke <daff@neyeon.com>
2016-10-14 16:20:08 -0700

    3.0.0

---
+++ dist/cjs/can-simple-map.js
@@ -0,0 +1,48 @@
+/*can-simple-map@3.0.0-pre.6#can-simple-map*/
+var Construct = require('can-construct');
+var canBatch = require('can-event/batch/batch');
+var canEvent = require('can-event');
+var assign = require('can-util/js/assign/assign');
+var types = require('can-util/js/types/types');
+var Observation = require('can-observation');
+var SimpleMap = Construct.extend({
+    setup: function () {
+        this._data = {};
+    },
+    init: function (initialData) {
+        this.attr(initialData);
+    },
+    attr: function (prop, value) {
+        var self = this;
+        if (arguments.length > 1) {
+            var old = this._data[prop];
+            this._data[prop] = value;
+            canBatch.trigger.call(this, prop, [
+                value,
+                old
+            ]);
+        } else if (typeof prop === 'object') {
+            Object.keys(prop).forEach(function (key) {
+                self.attr(key, prop[key]);
+            });
+        } else {
+            if (prop !== 'constructor') {
+                Observation.add(this, prop);
+                return this._data[prop];
+            }
+            return this.constructor;
+        }
+    }
+});
+assign(SimpleMap.prototype, canEvent);
+var oldIsMapLike = types.isMapLike;
+types.isMapLike = function (obj) {
+    if (obj instanceof SimpleMap) {
+        return true;
+    }
+    return oldIsMapLike.call(this, obj);
+};
+if (!types.DefaultMap) {
+    types.DefaultMap = SimpleMap;
+}
+module.exports = SimpleMap;
