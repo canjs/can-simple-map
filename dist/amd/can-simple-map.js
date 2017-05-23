8c4e0e97a2960521d0be9c4931ac56c3c0a347a4
Justin Meyer <justinbmeyer@gmail.com>
2017-05-23 10:56:14 -0500

    3.2.0-pre.1

---
+++ dist/amd/can-simple-map.js
@@ -0,0 +1,78 @@
+/*can-simple-map@3.2.0-pre.0#can-simple-map*/
+define(function (require, exports, module) {
+    var Construct = require('can-construct');
+    var canEvent = require('can-event');
+    var canBatch = require('can-event/batch');
+    var assign = require('can-util/js/assign');
+    var each = require('can-util/js/each');
+    var types = require('can-types');
+    var Observation = require('can-observation');
+    var SimpleMap = Construct.extend({
+        setup: function (initialData) {
+            this._data = {};
+            this.attr(initialData);
+        },
+        attr: function (prop, value) {
+            var self = this;
+            if (arguments.length === 0) {
+                Observation.add(this, '__keys');
+                var data = {};
+                each(this._data, function (value, prop) {
+                    Observation.add(this, prop);
+                    data[prop] = value;
+                }, this);
+                return data;
+            } else if (arguments.length > 1) {
+                var had = this._data.hasOwnProperty(prop);
+                var old = this._data[prop];
+                this._data[prop] = value;
+                canBatch.start();
+                if (!had) {
+                    canEvent.dispatch.call(this, '__keys', []);
+                }
+                canEvent.dispatch.call(this, prop, [
+                    value,
+                    old
+                ]);
+                canBatch.stop();
+            } else if (typeof prop === 'object') {
+                Object.keys(prop).forEach(function (key) {
+                    self.attr(key, prop[key]);
+                });
+            } else {
+                if (prop !== 'constructor') {
+                    Observation.add(this, prop);
+                    return this._data[prop];
+                }
+                return this.constructor;
+            }
+        },
+        serialize: function () {
+            var serialized = {};
+            Observation.add(this, '__keys');
+            each(this._data, function (data, prop) {
+                Observation.add(this, prop);
+                serialized[prop] = data && typeof data.serialize === 'function' ? data.serialize() : data;
+            }, this);
+            return serialized;
+        },
+        get: function () {
+            return this.attr.apply(this, arguments);
+        },
+        set: function () {
+            return this.attr.apply(this, arguments);
+        }
+    });
+    assign(SimpleMap.prototype, canEvent);
+    var oldIsMapLike = types.isMapLike;
+    types.isMapLike = function (obj) {
+        if (obj instanceof SimpleMap) {
+            return true;
+        }
+        return oldIsMapLike.call(this, obj);
+    };
+    if (!types.DefaultMap) {
+        types.DefaultMap = SimpleMap;
+    }
+    module.exports = SimpleMap;
+});
