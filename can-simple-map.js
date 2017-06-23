641f8af9cd983c28d51324f9eb50b7ce1af14416
Justin Meyer <justinbmeyer@gmail.com>
2017-06-22 22:07:20 -0500

    pointing at reflect apis (#29)
    * pointing at reflect apis
    
    * Update dist for release
    
    * Update dist for release
    
    * Add can-symbol symbols to simple map
    
    * Update dist for release
    
    * Add missing internal dependency file
    
    * Update dist for release
    
    * updating can-reflect/can-symbol
    
    * Update dist for release
    
    * making can-reflect a dependency since it is used directly in can-simple-map.js
    
    * Update dist for release
    
    * using the can-util version of single-reference
    
    * Update dist for release
    
    * Update deps for new can-types
    
    * Update dist for release
    
    * not setting deprecated types.isMapLike
    
    * Update dist for release

--- can-simple-map.js
+++ can-simple-map.js
@@ -5,6 +5,9 @@ var assign = require("can-util/js/assign/assign");
 var each = require("can-util/js/each/each");
 var types = require("can-types");
 var Observation = require("can-observation");
+var canSymbol = require("can-symbol");
+var canReflect = require("can-reflect");
+var singleReference = require("can-util/js/single-reference/single-reference");
 
 // this is a very simple can-map like object
 var SimpleMap = Construct.extend(
@@ -46,8 +49,8 @@ var SimpleMap = Construct.extend(
 			}
 			// 1 argument
 			else if(typeof prop === 'object') {
-				Object.keys(prop).forEach(function(key) {
-					self.attr(key, prop[key]);
+				canReflect.eachKey(prop, function(value, key) {
+					self.attr(key, value);
 				});
 			}
 			else {
@@ -79,17 +82,42 @@ var SimpleMap = Construct.extend(
 
 assign(SimpleMap.prototype, canEvent);
 
-var oldIsMapLike = types.isMapLike;
-types.isMapLike = function(obj) {
-	if(obj instanceof SimpleMap) {
-		return true;
-	}
-
-	return oldIsMapLike.call(this, obj);
-};
-
 if(!types.DefaultMap) {
 	types.DefaultMap = SimpleMap;
 }
+
+// Set up translations between can-event and can-reflect bindings.
+SimpleMap.prototype[canSymbol.for("can.onKeyValue")] = function(key, handler){
+	var translationHandler = function(ev, newValue, oldValue){
+		handler.call(this, newValue, oldValue);
+	};
+	singleReference.set(handler, this, translationHandler, key);
+
+	this.addEventListener(key, translationHandler);
+};
+
+SimpleMap.prototype[canSymbol.for("can.offKeyValue")] = function(key, handler){
+	this.removeEventListener(key, singleReference.getAndDelete(handler, this, key) );
+};
+
+// Setup other symbols
+SimpleMap.prototype[canSymbol.for("can.isMapLike")] = true;
+SimpleMap.prototype[canSymbol.for("can.isListLike")] = false;
+SimpleMap.prototype[canSymbol.for("can.isValueLike")] = false;
+SimpleMap.prototype[canSymbol.for("can.getKeyValue")] = SimpleMap.prototype.get;
+SimpleMap.prototype[canSymbol.for("can.setKeyValue")] = SimpleMap.prototype.set;
+SimpleMap.prototype[canSymbol.for("can.getValue")] = SimpleMap.prototype.get;
+SimpleMap.prototype[canSymbol.for("can.setValue")] = SimpleMap.prototype.set;
+SimpleMap.prototype[canSymbol.for("can.deleteKeyValue")] = function(prop) {
+	return this.attr(prop, undefined);
+};
+// SimpleMaps don't do dependent keys, so this is always false
+SimpleMap.prototype[canSymbol.for("can.keyHasDependencies")] = function(key) {
+	return false;
+};
+// ...and this is always empty
+SimpleMap.prototype[canSymbol.for("can.getKeyDependencies")] = function(key) {
+	return undefined;
+};
 
 module.exports = SimpleMap;
--- can-simple-map_test.js
+++ can-simple-map_test.js
@@ -2,6 +2,8 @@ var QUnit = require('steal-qunit');
 var SimpleMap = require('./can-simple-map');
 var compute = require('can-compute');
 var clone = require('steal-clone');
+var canSymbol = require('can-symbol');
+var canReflect = require('can-reflect');
 
 QUnit.module('can-simple-map');
 
@@ -80,4 +82,73 @@ QUnit.test("serialize and get are observable",2, function(){
 
 	map.set("foo","bar");
 
+});
+
+test("works with can-reflect", 8, function(){
+	var b = new SimpleMap({ "foo": "bar" });
+	// var c = new (SimpleMap.extend({
+	// 	"baz": canCompute(function(){
+	// 		return b.attr("foo");
+	// 	})
+	// }))({ "foo": "bar", thud: "baz" });
+
+	QUnit.equal( canReflect.getKeyValue(b, "foo"), "bar", "get value");
+
+	var handler = function(newValue){
+		QUnit.equal(newValue, "quux", "observed new value");
+
+		// Turn off the "foo" handler but "thud" should still be bound.
+		canReflect.offKeyValue(b, "foo", handler);
+	};
+	QUnit.ok(!canReflect.isValueLike(b), "isValueLike is false");
+	QUnit.ok(canReflect.isMapLike(b), "isMapLike is true");
+	QUnit.ok(!canReflect.isListLike(b), "isListLike is false");
+
+	QUnit.ok( !canReflect.keyHasDependencies(b, "foo"), "keyHasDependencies -- false");
+
+	canReflect.onKeyValue(b, "foo", handler);
+	// Do a second binding to check that you can unbind correctly.
+	canReflect.onKeyValue(b, "baz", handler);
+
+	b.attr("foo", "quux");
+
+	QUnit.equal( canReflect.getKeyValue(b, "foo"), "quux", "bound value");
+	// sanity checks to ensure that handler doesn't get called again.
+	b.attr("foo", "thud");
+	b.attr("baz", "quux");
+
+});
+
+QUnit.test("can-reflect setKeyValue", function(){
+	var a = new SimpleMap({ "a": "b" });
+
+	canReflect.setKeyValue(a, "a", "c");
+	QUnit.equal(a.attr("a"), "c", "setKeyValue");
+});
+
+QUnit.test("can-reflect getKeyDependencies", function() { 
+	var a = new SimpleMap({ "a": "a" });
+
+	ok(!canReflect.getKeyDependencies(a, "a"), "No dependencies before binding");
+
+});
+
+QUnit.test("registered symbols", function() { 
+	var a = new SimpleMap({ "a": "a" });
+
+	ok(a[canSymbol.for("can.isMapLike")], "can.isMapLike");
+	equal(a[canSymbol.for("can.getKeyValue")]("a"), "a", "can.getKeyValue");
+	a[canSymbol.for("can.setKeyValue")]("a", "b");
+	equal(a.attr("a"), "b", "can.setKeyValue");
+
+	function handler(val) {
+		equal(this, a);
+		equal(val, "c", "can.onKeyValue");
+	}
+
+	a[canSymbol.for("can.onKeyValue")]("a", handler);
+	a.attr("a", "c");
+
+	a[canSymbol.for("can.offKeyValue")]("a", handler);
+	a.attr("a", "d"); // doesn't trigger handler
 });
